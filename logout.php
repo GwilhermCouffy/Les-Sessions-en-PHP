b6ec9e29b38315156c1a6a8a77ba425280a992c9
Gwilherm Couffy <90566016+Hellnettles@users.noreply.github.com>
2021-11-09 10:01:04 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@ $_SESSION = array();
 
 if (isset($_COOKIE[session_name()])) 
 {
-    setcookie(session_name(),'', time()-4200, '/');
+    setcookie(session_name(),'',time()-4200, '/');
 }
 
 session_destroy();
